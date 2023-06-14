f7c61c982f21bdf4714e36ca9a76bfb97c3cfe64
IldarAbdulin <abdulinildar1@gmail.com>
2023-06-14 14:53:27 +0600

    передал файл variables с цветами, поменял шрифт, добавил иконку и удалил все старые файлы

--- src/App.js
+++ src/App.js
@@ -2,16 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { Categories } from './components';
-import PremiumPage from './pages/PremiumPage';
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
-        <Categories />
         <Routes>
-          <Route path={'/premium'} element={<PremiumPage/>}/>
         </Routes>
       </Provider>
     </BrowserRouter>
