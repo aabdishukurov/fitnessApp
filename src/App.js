02adea0b11863caab862672b9c6b24e0bc2d6daa
IldarAbdulin <abdulinildar1@gmail.com>
2023-06-14 18:37:12 +0600

    сверстал часть с категорями

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Categories } from './components';
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
         <Routes>
+          <Route path={`/categories`} element={<Categories />}/>
         </Routes>
       </Provider>
     </BrowserRouter>
