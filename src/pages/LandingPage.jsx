ca108fcf4f5e7bc929badb9d877914d449b737d9
Swibm <swibm9009@gmalil.com>
2023-06-18 21:51:44 +0600

    added pro subscription component

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Categories from '../components/ildar/categories/Categories'
+import { ProDescription } from '../components/ProDescription'
 
 const LandingPage = () => {
     return (
         <div>
             <h1>Landing</h1>
             <Categories />
+            <ProDescription/>
         </div>
     )
 }
