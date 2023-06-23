580a01671e010dc5c0875254f0b21e4e5b6af4cd
Akunbekovv <akunbekovmaksatt@gmail.com>
2023-06-23 22:14:29 +0600

    added form water

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PageCategory } from './pages';
+import WaterCalculator from "./components/maksatbek/waterCalculator/WaterCalculator";
 
 function App() {
   return (
     <BrowserRouter>
       <Provider store={store}>
         <Routes>
+            <Route index element={<WaterCalculator/>}/>
           <Route path={`/categories`} element={<PageCategory />}/>
         </Routes>
       </Provider>
