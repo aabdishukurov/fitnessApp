be591d5d30eb4dde7ed15e18859d5548ce8f9128
IldarAbdulin <abdulinildar1@gmail.com>
2023-06-25 16:57:17 +0600

    переделал часть с категориями по новому дизайну, добавил в папку UI файл Button, добавил в компоненты новую папку Title работает как переиспользуемый компонент для заголовков

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import { Pro } from "./ivan/Pro/Pro";
 import { ProCard } from "./ivan/ProCard/ProCard";
 import { Nutritios } from "./ivan/Nutritios/Nutritios";
 import { Nutritio } from "./ivan/Nutritio/Nutritio";
+import { Title } from "./title/Title";
 
-export { Category, ProCard, Pro, Nutritios, Nutritio };
+export { Category, ProCard, Pro, Nutritios, Nutritio, Title };
