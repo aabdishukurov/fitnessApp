4fe244262d37734a6a085c59ae48d53750f4d310
AprioriDev <apriorifox.dev@gmail.com>
2023-07-03 17:18:15 +0600

    Стилизацию ззакончил на сколько мог, дальше только с бэком

--- src/App.js
+++ src/App.js
@@ -3,13 +3,26 @@ import { Provider } from 'react-redux';
 import { store } from './store';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PageCategory, PageHome, PageProHome } from './pages';
+import Timer from './components/timer/Timer';
+import ExerPage from './pages/exerPage/ExerPage';
 
 function App() {
+
+
+  const test = {
+    name: 'Глаз',
+    cons: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis eius est ducimus praesentium quibusdam unde doloribus excepturi illum facilis eligendi, fuga harum perferendis ratione aliquid ab qui nemo ipsum similique '
+  }
+
+
+
   return (
     <BrowserRouter>
       <Provider store={store}>
         <Routes>
           <Route path={`/`} element={<PageHome />} />
+          <Route path={`/timer`} element={<Timer />} />
+          <Route path={`/ex`} element={<ExerPage test = {test}/>} />
           <Route path={`/pro`} element={<PageProHome />} />
           <Route path={`/categories`} element={<PageCategory />}/>
         </Routes>
--- src/pages/PageHome.jsx
+++ src/pages/PageHome.jsx
@@ -1,4 +1,5 @@
 import { Pro } from "../components";
+import Timer from "../components/timer/Timer";
 
 export const PageHome = () => {
   return (
